20168a2f5a7114988e3f6dd0c9ced31ff34076bc
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2021-07-22 07:22:54 -0600

    Update Tweet.js

--- random-quote-machine/src/Components/Tweet.js
+++ random-quote-machine/src/Components/Tweet.js
@@ -1,5 +1,5 @@
 const Tweet = (props) => {
-   return( <a href="https://twitter.com/intent/tweet" id="tweet-quote" className='btn btn-info'><i className="fab fa-twitter"></i></a>
+   return( <a href={"https://twitter.com/intent/tweet?text="+encodeURIComponent(props.quote)+encodeURIComponent(props.author)} id="tweet-quote" className='btn btn-info'><i className="fab fa-twitter"></i></a>
    );
 };
 
